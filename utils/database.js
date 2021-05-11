a155470a6af75a7bb6d5d8f433b7aa53a5975f82
Polaris Taki <zyyeh0120@gmail.com>
2021-05-11 20:09:57 +0800

    v1.03

--- utils/database.js
+++ utils/database.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host:'localhost',
-  user: 'root',
-  password: '0000',
-  database: 'crown_36'});
+  user: 'bab40537af1644',
+  password: '41a7b5a2',
+  database: 'heroku_ed17c54729106d8'});
 
 module.exports = pool.promise();
