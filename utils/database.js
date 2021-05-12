769e69629ec0110810334a77762c3601be7be932
Polaris Taki <zyyeh0120@gmail.com>
2021-05-12 17:07:47 +0800

    v/crown3_36

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host:'localhost',
+  host:'us-cdbr-east-03.cleardb.com',
   user: 'bab40537af1644',
   password: '41a7b5a2',
   database: 'heroku_ed17c54729106d8'});
